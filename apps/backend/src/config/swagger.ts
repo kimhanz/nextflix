bf8abe251a6bf83e7ebd437ed32afdfa276791cd
Kim Hanz <nanaka3@hotmail.com>
2025-10-06 20:42:39 +0700

    backend: config api/docs

--- apps/backend/src/config/swagger.ts
+++ apps/backend/src/config/swagger.ts
@@ -10,5 +10,5 @@ export function setupSwagger(app: INestApplication) {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 }
